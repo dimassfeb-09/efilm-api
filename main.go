493d93e83fa2668befbb36fd608d69eb9c736c97
Dimas Febriyanto <dimasfebriyanto@pop-os.localdomain>
2023-08-10 18:07:01 +0700

    docker

--- main.go
+++ main.go
@@ -7,25 +7,6 @@ import (
 	"log"
 )
 
-//	@title			Swagger API E-Film
-//	@version		1.0
-//	@description	This is a sample server film
-//	@termsOfService	http://swagger.io/terms/
-
-//	@contact.name	API Support
-//	@contact.url	http://www.swagger.io/support
-//	@contact.email	support@swagger.io
-
-//	@license.name	Apache 2.0
-//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-
-//	@host		localhost:8080
-//	@BasePath	/api
-
-//	@securityDefinitions.basic	BasicAuth
-
-//	@externalDocs.description	OpenAPI
-//	@externalDocs.url			https://swagger.io/resources/open-api/
 func main() {
 	r := gin.Default()
 	r.HandleMethodNotAllowed = true
